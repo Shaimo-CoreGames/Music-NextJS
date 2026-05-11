a282ee20e550a7e656b61034633b742e3d78c2b5
SHAH MEER <shahmeer.codes97@gmail.com>
2026-05-11 15:51:58 +0500

    FeaturedSection Done!

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
+import FeaturedCourses from "../components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturedCourses />
       <h1 >
       </h1>
     </main>
