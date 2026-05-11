365537a89998ad2f9855b3f0bc541f6c1528a448
SHAH MEER <shahmeer.codes97@gmail.com>
2026-05-11 16:26:59 +0500

    Designing Sticky Scroll Done!

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
 import FeaturedCourses from "../components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
       <h1 >
       </h1>
     </main>
