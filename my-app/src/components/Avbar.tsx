c82aed4cf6c34824f4ba692fb23c84bd892d9988
SHAH MEER <shahmeer.codes97@gmail.com>
2026-05-12 09:20:07 +0500

    Contact Page Done!

--- my-app/src/components/Avbar.tsx
+++ my-app/src/components/Avbar.tsx
@@ -12,33 +12,38 @@ function Navbar({ className }: { className?: string }) {
 
     return (
 
-        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-100", className)}>
-
-            <Menu setActive={setActive} >
-                <Link href="/">
-                    <MenuItem setActive={setActive} active={active} item="home">
+        <div
+            className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                    <MenuItem setActive={setActive} active={active} item="Home">
 
                     </MenuItem>
                 </Link>
-
-                <MenuItem setActive={setActive} active={active} item="Courses">
-                    <div className="flex flex-col gap-2 text-sm">
-                        <HoveredLink href="/courses">Our Courses</HoveredLink>
-                        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-                        <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+                <MenuItem
+                    setActive={setActive} active={active} item="Our Courses"
+                >
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/courses">All Courses</HoveredLink>
+                        <HoveredLink href="/courses">
+                            Basic Music Theory
+                        </HoveredLink>
+                        <HoveredLink href="/courses">
+                            Advanced Composition
+                        </HoveredLink>
                         <HoveredLink href="/courses">Songwriting</HoveredLink>
-                        <HoveredLink href="/courses">Music Production</HoveredLink>
+                        <HoveredLink href="/courses">
+                            Music Production
+                        </HoveredLink>
                     </div>
                 </MenuItem>
-
-                <Link href="/contact">
+                <Link href={"/contact"}>
                     <MenuItem setActive={setActive} active={active} item="Contact Us">
 
                     </MenuItem>
                 </Link>
             </Menu>
-
-
         </div>
     )
 }
